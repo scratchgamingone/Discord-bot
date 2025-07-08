15676292b54484eb1269c39147b41757d3f9e7c6
scratchgamingone <vader50roan@gmail.com>
2025-07-08 14:32:23 -0400

    This is a test part 3

--- upload to git.bat
+++ upload to git.bat
@@ -1,21 +1,21 @@
 @echo off
-echo Uploading Discord Bot to GitHub...
+echo Updating Discord Bot on GitHub...
 
-:: Initialize Git repository (if not already initialized)
-git init
-
-:: Add all files to the repository
+:: Add all new and changed files to the repository
 git add .
 
 :: Commit changes
 set /p commit_message=Enter commit message: 
 git commit -m "%commit_message%"
 
-:: Set the remote repository URL
-git remote add origin https://github.com/scratchgamingone/Discord-bot.git
+:: Ensure we're on the main branch
+git checkout main
+
+:: Pull any changes from the remote repository
+git pull origin main
 
 :: Push to GitHub
-git push -u origin master
+git push -u origin main
 
-echo Upload complete!
+echo Update complete!
 pause
